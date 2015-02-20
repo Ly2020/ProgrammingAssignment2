3bcc1d46c8fe69ef9dcae334b45f47324b6cc1be
Ly2020 <Ly2020@Kevins-Air.home>
2015-02-19 20:19:33 -0500

    Cache Matrix R file

---
+++ cachematrix.R
@@ -0,0 +1,33 @@
+## Two R functions to cache potentially time-consuming computations.
+
+
+## `makeCacheMatrix`: This function creates a special "matrix" object
+##  that can cache its inverse
+
+makeCacheMatrix <- function(x = matrix()) {
+        m <- NULL
+        set <- function(y){
+                x <<- y
+                m <<- NULL 
+        }
+        get <- function() x
+        setmean <- function(mean) m <<- mean
+        getmean <- function() m
+        list(set = set, get = get, setmean = setmean, getmean = getmean)
+}
+
+## `cacheSolve`: This function returns a matrix that is the inverse of 'x'. 
+##  If the inverse has already been calculated (and the matrix has not changed), 
+##  then `cacheSolve` should retrieve the inverse from the cache.
+
+cacheSolve <- function(x, ...) {
+        m <- x$getmean()
+        if(!is.null(m)) {
+                message("getting cached data")
+                return(m)
+        }
+        data <- x$get()
+        m <- solve(data, ...)
+        x$setmean(m)
+        m
+}
